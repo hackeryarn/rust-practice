8e6afbd94a07e65138dfe10fde37f59be2318403
achernyak <artemchernyak@gmail.com>
2016-03-28 11:25:20 -0500

    diverges and pointers

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,7 +1,9 @@
 fn main() {
+    let f: fn(i32) -> i32 = add_one;
     print_number(5);
     print_sum(5, 6);
     print_add_one(5);
+    diverges();
 }
 
 fn print_number(x: i32) {
@@ -14,6 +16,10 @@ fn print_sum(x: i32, y: i32) {
 
 fn print_add_one(x: i32) {
     println!("add one is: {}", add_one(x));
+}
+
+fn diverges() -> ! {
+    panic!("This function never returns!");
 }
 
 fn add_one(x: i32) -> i32 {
