3fa8e3582948c38527e312a2e7946605e11de9e6
achernyak <artemchernyak@gmail.com>
2016-03-28 16:07:57 -0500

    basic struct

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,17 @@
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let origin = Point { x: 0, y: 0 };
+
+    println!("The origin is at ({}, {})", origin.x, origin.y);
+
+    let mut point = Point { x: 0, y: 0 };
+
+    point.x = 5;
+
+    println!("The point is at ({}, {})", point.x, point.y);
 }
