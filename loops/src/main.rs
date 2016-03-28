f3bf60c86e70fec7538ea9b13b267df28cf2c502
achernyak <artemchernyak@gmail.com>
2016-03-28 12:09:17 -0500

    loops

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     while_loop();
     for_loop();
+    just_loop();
+    odd_combo();
 }
 
 fn while_loop() {
@@ -23,7 +25,34 @@ fn for_loop() {
         println!("{}", x);
     }
 
-    for (i,j) in (5..10).enumerate() {
+    for (i, j) in (5..10).enumerate() {
         println!("i = {} and j = {}", i, j);
+    }
+}
+
+fn just_loop() {
+    let mut x = 5;
+    loop {
+        x += x - 3;
+
+        println!("{}", x);
+
+        if x % 5 == 0 {
+            break;
+        }
+    }
+}
+
+fn odd_combo() {
+    'outer: for x in 0..10 {
+        'inner: for y in 0..10 {
+            if x % 2 == 0 {
+                continue 'outer;
+            }
+            if y % 2 == 0 {
+                continue 'inner;
+            }
+            println!("x: {}, y: {}", x, y);
+        }
     }
 }
