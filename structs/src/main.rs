52d0c54e21ffe823536672d011a8fb505a1d82ec
achernyak <artemchernyak@gmail.com>
2016-03-28 16:18:34 -0500

    structs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -5,8 +5,16 @@ struct Point {
 
 struct PointRef<'a> {
     x: &'a mut i32,
-    y: &'a mut i32
+    y: &'a mut i32,
 }
+
+struct Point3d {
+    x: i32,
+    y: i32,
+    z: i32,
+}
+
+struct Inches(i32);
 
 fn main() {
     let origin = Point { x: 0, y: 0 };
@@ -27,4 +35,16 @@ fn main() {
     }
 
     println!("The new point is at ({}, {})", point.x, point.y);
+
+    let mut point3d = Point3d { x: 0, y: 0, z: 0 };
+
+    point3d = Point3d { y: 1, .. point3d };
+
+    println!("The 3d point is at ({}, {}, {})", point3d.x, point3d.y, point3d.z);
+
+    let length = Inches (10);
+
+    let Inches(integer_length) = length;
+
+    println!("length is {} inches", integer_length);
 }
