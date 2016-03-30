9162487478a2b7b6c56c9563bfd63569e6f5b2c1
achernyak <artemchernyak@gmail.com>
2016-03-30 13:22:43 -0500

    builder pattern

--- method/src/main.rs
+++ method/src/main.rs
@@ -14,8 +14,44 @@ impl Circle {
     }
 }
 
+struct CircleBuilder {
+    x: f64,
+    y: f64,
+    radius: f64,
+}
+
+impl CircleBuilder {
+    fn new() -> CircleBuilder {
+        CircleBuilder { x: 0.0, y: 0.0, radius: 1.0, }
+    }
+
+    fn x(&mut self, coordinate: f64) -> &mut CircleBuilder {
+        self.x = coordinate;
+        self
+    }
+
+    fn y(&mut self, coordinate: f64) -> &mut CircleBuilder {
+        self.y = coordinate;
+        self
+    }
+
+    fn radius(&mut self, radius: f64) -> &mut CircleBuilder {
+        self.radius = radius;
+        self
+    }
+
+    fn finalize(&self) -> Circle {
+        Circle { x: self.x, y: self.y, radius: self.radius }
+    }
+}
+
 fn main() {
-    let c = Circle { x: 0.0, y: 0.0, radius: 2.0 };
+    let c = CircleBuilder::new()
+                .x(1.0)
+                .y(2.0)
+                .radius(2.0)
+                .finalize();
+
     println!("{}", c.area());
 
     let d = c.grow(2.0).area();
