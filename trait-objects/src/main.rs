a89bad0bb2adbb2b91eb862bb43fc9fea409bdf2
achernyak <artemchernyak@gmail.com>
2016-03-30 13:53:16 -0500

    setup

--- trait-objects/src/main.rs
+++ trait-objects/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+trait Foo {
+    fn method(&self) -> String;
+}
+
+
+impl Foo for u8 {
+    fn method(&self) -> String { format!("u8: {}", *self) }
+}
+
+impl Foo for String {
+    fn method(&self) -> String { format!("string: {}", *self) }
 }
